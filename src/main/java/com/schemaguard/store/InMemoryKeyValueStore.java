3bf944ef8260ddaf82cfafbce20eb5ed59547b34
PragatiAN1109 <narote.p@northeastern.edu>
2026-02-07 10:59:00 -0500

    fix: Add @Primary annotation to InMemoryKeyValueStore

--- src/main/java/com/schemaguard/store/InMemoryKeyValueStore.java
+++ src/main/java/com/schemaguard/store/InMemoryKeyValueStore.java
@@ -2,6 +2,7 @@ package com.schemaguard.store;
 
 import com.schemaguard.model.StoredDocument;
 import com.schemaguard.util.EtagUtil;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
+@Primary
 @Profile("!redis")
 public class InMemoryKeyValueStore implements KeyValueStore {
 
