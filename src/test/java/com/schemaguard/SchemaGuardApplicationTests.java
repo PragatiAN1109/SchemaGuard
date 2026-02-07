318b8b5d5a27ecd2c854130d8e349319839cbe4d
PragatiAN1109 <narote.p@northeastern.edu>
2026-02-07 10:57:26 -0500

    fix: Add test profile to SchemaGuardApplicationTests

--- src/test/java/com/schemaguard/SchemaGuardApplicationTests.java
+++ src/test/java/com/schemaguard/SchemaGuardApplicationTests.java
@@ -2,8 +2,10 @@ package com.schemaguard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SchemaGuardApplicationTests {
 
     @Test
