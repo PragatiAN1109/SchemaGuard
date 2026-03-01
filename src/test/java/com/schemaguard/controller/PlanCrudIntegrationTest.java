a5765fb92557fe3ac24779f2a211207e7b6cad70
PragatiAN1109 <narote.p@northeastern.edu>
2026-02-28 19:22:57 -0500

    fix: test cases fix

--- src/test/java/com/schemaguard/controller/PlanCrudIntegrationTest.java
+++ src/test/java/com/schemaguard/controller/PlanCrudIntegrationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@WithMockUser  // Simulates an authenticated user for all tests in this class
 class PlanCrudIntegrationTest {
 
     @Autowired
